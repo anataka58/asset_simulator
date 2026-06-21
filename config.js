862a0da5665e023553ed037dc1b51944a605e8eb
anataka58 <anataka.28111058@gmail.com>
2026-06-21 21:58:58 +0900

    config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 
 window.ASSET_SIMULATOR_CONFIG = {
   googleSheets: {
-    assetsCsvUrl: "",
-    settingsCsvUrl: "",
-    scenariosCsvUrl: ""
+    assetsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGhQMVnYN3Bl1LjOd3JSqY8cuJ5sP3Io-rECmellCxQJXSFqEq2yBXMxjanXzekIvxl5mFhje0OQwG/pub?gid=0&single=true&output=csv",
+    settingsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGhQMVnYN3Bl1LjOd3JSqY8cuJ5sP3Io-rECmellCxQJXSFqEq2yBXMxjanXzekIvxl5mFhje0OQwG/pub?gid=936971869&single=true&output=csv",
+    scenariosCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSGhQMVnYN3Bl1LjOd3JSqY8cuJ5sP3Io-rECmellCxQJXSFqEq2yBXMxjanXzekIvxl5mFhje0OQwG/pub?gid=85692318&single=true&output=csv"
   }
 };
